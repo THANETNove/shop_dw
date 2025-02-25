e582184f03c394359d369ef98c884ace6759e34a
THANETNove <novel2020manga@gmail.com>
2025-02-25 23:30:07 +0700

    add

--- home.php
+++ home.php
@@ -1,13 +1,14 @@
 <?php
 session_start(); // เริ่มต้น session
 
+
 // ตรวจสอบว่าผู้ใช้ล็อกอินหรือไม่
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['username'])) {
     header("Location: index.php"); // ถ้ายังไม่ login ให้ไปหน้า login
     exit();
 }
 ?>
-?>
+
 
 
 <!DOCTYPE html>
